7ea9db994cd3bc3fbfbd44c4af67b4b50035ff22
Jeff Cheng <jeffcheng777@gmail.com>
2021-10-19 03:29:18 +0800

    modularized the code

--- jokes.py
+++ jokes.py
@@ -2,31 +2,39 @@ import requests
 import requests
 from random import choice
 
-user_input = input("What would you like to search for? ")
-url = "https://icanhazdadjoke.com/search"
-response = requests.get(
-        url,
-        headers={"Accept": "application/json"},
-        params={"term": user_input}
-    ).json()
+def main():
+    user_input = input("What would you like to search for? ('0' for exit) ")
+    if user_input == "0":
+        return 1
+    url = "https://icanhazdadjoke.com/search"
+    response = requests.get(
+            url,
+            headers={"Accept": "application/json"},
+            params={"term": user_input}
+        ).json()
 
-num_jokes = response["total_jokes"]
-results = response["results"]
-if num_jokes > 1:
-    answer = ""
-    while True:
-        answer = input(f"I found {num_jokes} jokes about {user_input}. Would you like to show them all? (Y/N)").lower()
-        if answer == "y" or answer == "n":
-            break
+    num_jokes = response["total_jokes"]
+    results = response["results"]
+    if num_jokes > 1:
+        answer = ""
+        while True:
+            answer = input(f"I found {num_jokes} jokes about {user_input}. Would you like to show them all? (Y/N)").lower()
+            if answer == "y" or answer == "n":
+                break
+            else:
+                print("Please enter 'Y' or 'N'")
+        if answer == "y":
+            for result in results:
+                print(result["joke"])
         else:
-            print("Please enter 'Y' or 'N'")
-    if answer == "y":
-        for result in results:
-            print(result["joke"])
+            print(f'Here\'s one: \n{choice(results)["joke"]}')
+    elif num_jokes == 1:
+        print(f"I found one joke about {user_input}")
+        print(response["results"][0]['joke'])
     else:
-        print(f'Here\'s one: \n{choice(results)["joke"]}')
-elif num_jokes == 1:
-    print(f"I found one joke about {user_input}")
-    print(response["results"][0]['joke'])
-else:
-    print(f"Sorry, couldn't find a joke with your term: {user_input} ")
+        print(f"Sorry, couldn't find a joke with your term: {user_input} ")
+
+if __name__ == '__main__':
+    while True:
+        if main() == 1:
+            break
